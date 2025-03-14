6c9ab7ae2c77434b2b556b95770ca40aa3fe2345
messamat <mathis.messager@mail.mcgill.ca>
2025-03-14 14:28:03 +0100

    add folder structure in set_up.py

---
+++ src/set_up.py
@@ -0,0 +1,8 @@
+import os
+from pathlib import Path
+
+#Set up project directory structure
+rootdir = os.path.dirname(os.getcwd())
+datdir = Path(rootdir, "data")
+resdir = Path(rootdir, "results")
+srcdir = Path(rootdir, 'src')
