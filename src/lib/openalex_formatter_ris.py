0ac7e4f6912cb8d93b22ce4be3de74ae4fe5f7a9
messamat <mathis.messager@mail.mcgill.ca>
2025-03-22 20:29:31 +0100

    Correct export_oalex_works_to_ris with openalex_formatter

---
+++ src/lib/openalex_formatter_ris.py
@@ -0,0 +1,107 @@
+import tempfile
+from io import StringIO
+from nameparser import HumanName
+
+# from formats.util import paginate, get_nested_value, get_first_page, \
+#     unravel_index
+from openalex_formatter_util import get_nested_value, unravel_index #paginate, get_first_page
+
+
+RIS_CONTENT_TYPE = 'text/x-ris'
+
+type_map = {
+    "article": "JOUR",
+    'review': 'JOUR',
+    'preprint': 'JOUR',
+    "book-chapter": "CHAP",
+    "dissertation": "THES",
+    "book": "BOOK",
+    "dataset": "DATA",
+    "paratext": "GEN",
+    "other": "GEN",
+    "reference-entry": "ENTRY",
+    "report": "RPRT",
+    "peer-review": "JOUR",
+    "standard": "STAND",
+    'supplementary-materials': 'FIGURE',
+    "editorial": "JOUR",
+    "erratum": "ERRT",
+    "grant": "GEN",
+    "letter": "LETTER",
+}
+
+
+def build_ris_entry(work):
+    ris_entry = [f"TY  - {type_map[work['type']]}",
+                 f"TI  - {work['title']}",
+                 f"PY  - {work['publication_year']}"]
+
+    if get_nested_value(work, 'primary_location', 'source',
+                        'host_organization_name'):
+        ris_entry.append(
+            f"PB  - {work['primary_location']['source']['host_organization_name']}")
+
+    if get_nested_value(work, 'primary_location', 'source', 'issn_l'):
+        ris_entry.append(
+            f"SN  - {work['primary_location']['source']['issn_l']}")
+
+    if get_nested_value(work, 'primary_location', 'source', 'display_name'):
+        ris_entry.append(
+            f"T2  - {work['primary_location']['source']['display_name']}")
+
+    if work['ids'].get('doi'):
+        doi = work['ids']['doi'].split('.org/')[-1]
+        ris_entry.append(f"DO  - {doi}")
+        ris_entry.append(f"UR  - {work['ids']['doi']}")
+
+    if work['publication_date']:
+        ris_entry.append(f"DA  - {work['publication_date']}")
+
+    # Authors
+    for authorship in work['authorships']:
+        author_name = authorship['author']['display_name']
+        parsed_name = HumanName(author_name)
+        ris_entry.append(f"AU  - {parsed_name.last}, {parsed_name.first}")
+        if authorship['raw_affiliation_strings']:
+            for affiliation in authorship['raw_affiliation_strings']:
+                ris_entry.append(f"C1  - {affiliation}")
+
+    # Additional optional fields
+    if work['language']:
+        ris_entry.append(f"LA  - {work['language']}")
+    if work['keywords']:
+        for kw in work['keywords']:
+            ris_entry.append(f"KW  - {kw['display_name']}")
+    if work['biblio'].get('volume'):
+        ris_entry.append(f"VL  - {work['biblio']['volume']}")
+    if work['biblio'].get('issue'):
+        ris_entry.append(f"IS  - {work['biblio']['issue']}")
+    if work['biblio'].get('first_page'):
+        ris_entry.append(f"SP  - {work['biblio']['first_page']}")
+    if work['biblio'].get('last_page'):
+        ris_entry.append(f"EP  - {work['biblio']['last_page']}")
+
+    if work.get('abstract_inverted_index') and (work.get('open_access') or {}).get('is_oa'):
+        ris_entry.append(f"AB  - {unravel_index(work['abstract_inverted_index'])}")
+
+    ris_entry.append('ER  -\n')
+
+    return "\n".join(ris_entry)
+
+
+# def export_ris(export):
+#     fname = tempfile.mkstemp(suffix='.ris')[1]
+#     with open(fname, 'w') as f:
+#         for page in paginate(export, fname):
+#             for work in page:
+#                 ris_entry = build_ris_entry(work)
+#                 f.write(ris_entry)
+#     return fname
+
+# def instant_export(export):
+#     first_page = get_first_page(export)
+#     buffer = StringIO()
+#     for work in first_page['results']:
+#         ris_entry = build_ris_entry(work)
+#         buffer.write(ris_entry)
+#     return buffer.getvalue()
---
+++ src/lib/openalex_formatter_util.py
@@ -0,0 +1,232 @@
+import datetime
+import itertools
+from math import ceil
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
+
+import pandas as pd
+import requests
+from requests import JSONDecodeError
+
+#from app import db, logger
+
+TRUNCATE_MAX_CHARS = 32_740
+
+WORKS_DF_KEY = 'works'
+
+
+# def update_export_progress(export, progress):
+#     export.progress = progress
+#     export.progress_updated = datetime.datetime.utcnow()
+#     db.session.merge(export)
+#     db.session.commit()
+
+
+# def construct_query_url(cursor, export, per_page):
+#     parsed_query_url = urlparse(export.query_url)
+#     query_args = parse_qs(parsed_query_url.query)
+#     query_args['cursor'] = cursor
+#     query_args['per_page'] = per_page
+#     parsed_query_url = parsed_query_url._replace(
+#         query=urlencode(query_args, doseq=True)
+#     )
+#     query_url = urlunparse(parsed_query_url)
+#     return query_url
+
+
+# def paginate(export, fname=None, max_results=200 * 250):
+#     page = 1
+#     cursor = '*'
+#     per_page = 200
+#     results_count = 0
+
+#     s = requests.session()
+
+#     while results_count <= max_results and cursor is not None:
+#         query_url = construct_query_url(cursor, export, per_page)
+#         try:
+#             r = s.get(query_url)
+#             j = r.json()
+#         except JSONDecodeError:
+#             per_page = ceil(per_page / 2)
+#             continue
+#         per_page = min(200, per_page * 2)
+#         total_count = j['meta']['count']
+#         cursor = j['meta']['next_cursor']
+#         results = j['results']
+#         results_count += len(results)
+
+#         yield results
+
+#         if not export.args.get('is_async'):
+#             update_export_progress(export, 1)
+#             break
+
+#         update_export_progress(export, results_count / total_count)
+#         if fname:
+#             logger.info(f'wrote {results_count}/{total_count} to {fname}')
+#         page += 1
+
+
+def get_nested_value(work, *keys):
+    for key in keys:
+        if work is None or not isinstance(work, dict):
+            return None
+        work = work.get(key)
+    return work
+
+
+def parse_bool(s):
+    if s.lower() in ["true", "yes", "t", "on", "1"]:
+        return True
+    elif s.lower() in ["false", "no", "f", "off", "0"]:
+        return False
+    else:
+        raise ValueError("Invalid boolean value: {}".format(s))
+
+
+def unravel_index(inverted_index):
+    unraveled = {}
+    for key, values in inverted_index.items():
+        for value in values:
+            unraveled[value] = key
+
+    sorted_unraveled = dict(sorted(unraveled.items()))
+    result = " ".join(sorted_unraveled.values()).replace("\n", "")
+    return result
+
+
+def get_first_page(export):
+    params = {
+        'page': '1',
+        'per-page': '200'
+    }
+    return requests.get(export.query_url, params=params).json()
+
+
+def truncate_format_str(cell_str):
+    cell_str = cell_str[: TRUNCATE_MAX_CHARS - 3]
+    if len(cell_str) >= TRUNCATE_MAX_CHARS - 3:
+        cell_str += '...'
+    return cell_str
+
+
+def truncate_format_row(row):
+    for k in row.keys():
+        row[k] = truncate_format_str(row[k]) if isinstance(row[k], str) else \
+            row[k]
+    return row
+
+
+def object_columns_select(export_columns):
+    m = {}
+    for column in export_columns:
+        split = column.split('.', maxsplit=1)
+        if len(split) == 2:
+            m[split[0]] = split[1]
+        else:
+            m[split[0]] = ''
+    return m
+
+
+def set_work_ids(col_list, df):
+    for i, _list in enumerate(col_list):
+        for obj in _list:
+            obj['work_id'] = df['id'].iloc[i]
+
+
+def set_column_order(sub_df):
+    front_cols = ['work_id']
+    if 'id' in sub_df.columns:
+        front_cols.insert(0, 'id')
+    end_cols = [col for col in sub_df.columns if col not in front_cols]
+    df = sub_df[front_cols + end_cols]
+    return df
+
+
+def truncate_string(x, max_len=TRUNCATE_MAX_CHARS):
+    return x[:max_len] if isinstance(x, str) else x
+
+
+def join_lists(cell):
+    if isinstance(cell, list) and all(
+            isinstance(i, (str, int, float)) for i in cell):
+        return '|'.join(cell)
+    return cell
+
+
+def reconstruct_abstract(row, inverted_columns):
+    word_positions = {}
+    for col in inverted_columns:
+        word = col.split('.', maxsplit=1)[-1]
+        indexes = row[col]
+        if not isinstance(indexes, list):
+            continue
+        for index in indexes:
+            word_positions[index] = word
+    # Create a list of words in correct order
+    max_index = max(word_positions.keys(), default=-1)
+    abstract = [word_positions.get(i, '') for i in range(max_index + 1)]
+    return ' '.join(abstract)
+
+
+def build_dataframes(export):
+    dfs = dict()
+    raw_columns = ['id']
+    columns_map = {}
+    for page in paginate(export):
+        df = pd.json_normalize(page)
+        drop_columns = [col for col in df.columns if
+                        'abstract_inverted' in col]
+        if 'open_access.is_oa' in df.columns:
+            df['abstract'] = df.apply(reconstruct_abstract,
+                                      inverted_columns=drop_columns,
+                                      axis=1)
+            df.loc[~df['open_access.is_oa'], 'abstract'] = ''
+        df.drop(columns=drop_columns, inplace=True)
+        export_cols = export.args.get('columns')
+        if export_cols:
+            raw_columns.extend(export_cols.split(','))
+            columns_map = object_columns_select(raw_columns)
+            drop_columns = [col for col in df.columns if
+                            col not in list(
+                                columns_map.keys()) + raw_columns]
+            df.drop(columns=drop_columns, inplace=True)
+        if WORKS_DF_KEY not in dfs:
+            dfs[WORKS_DF_KEY] = df
+        else:
+            dfs[WORKS_DF_KEY] = pd.concat([dfs[WORKS_DF_KEY], df],
+                                          axis=0).reset_index(drop=True)
+        for col in df.columns:
+            filtered_series = df[col].dropna().apply(
+                lambda x: x if isinstance(x, list) else [])
+            non_empty_lists = filtered_series[filtered_series.map(len) > 0]
+            if not non_empty_lists.empty and isinstance(
+                    non_empty_lists.iloc[0][0], dict):
+                col_list_form = df[col].apply(lambda x: x if isinstance(x, list) else []).tolist()
+                set_work_ids(col_list_form, df)
+                sub_df = pd.json_normalize(
+                    list(itertools.chain(*col_list_form)))
+                sub_df = set_column_order(sub_df)
+                if export.args.get('columns'):
+                    drop_columns = [column for column in sub_df.columns if
+                                    column not in [columns_map.get(col, [])] + [
+                                        'work_id']]
+                    sub_df.drop(columns=drop_columns, inplace=True)
+                dfs[WORKS_DF_KEY].drop(columns=[col], inplace=True)
+                if col in dfs:
+                    dfs[col] = pd.concat([dfs[col], sub_df],
+                                         axis=0).reset_index(drop=True)
+                else:
+                    dfs[col] = sub_df
+        drop_columns = [key for key in dfs.keys() if
+                        key in dfs[WORKS_DF_KEY].columns]
+        if drop_columns:
+            dfs[WORKS_DF_KEY].drop(columns=drop_columns, inplace=True)
+    if export.args.get('columns'):
+        drop_columns = [col for col in dfs[WORKS_DF_KEY].columns if
+                        col not in raw_columns]
+        dfs[WORKS_DF_KEY].drop(columns=drop_columns, inplace=True)
+    if export.args.get('truncate'):
+        for k in dfs.keys():
+            dfs[k] = dfs[k].applymap(truncate_string)
+    return dfs
