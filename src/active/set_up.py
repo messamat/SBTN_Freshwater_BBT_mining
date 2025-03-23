7d6d1698c776e0ce5782fcc6fe3b74e36fe9b723
messamat <mathis.messager@mail.mcgill.ca>
2025-03-23 20:57:22 +0100

    Switch to different file structure for jbook

--- src/active/set_up.py
+++ src/active/set_up.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 #Set up project directory structure
-rootdir = os.path.dirname(os.getcwd())
+rootdir = os.path.dirname((os.path.dirname(os.getcwd())))
 datdir = Path(rootdir, "data")
 resdir = Path(rootdir, "results")
 srcdir = Path(rootdir, 'src')
