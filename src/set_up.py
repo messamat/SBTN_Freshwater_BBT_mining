027e4c438a8519ae96e2ec7bdefabcee3f8d12b4
messamat <mathis.messager@mail.mcgill.ca>
2025-03-25 11:27:51 +0100

    force merge to master

--- src/set_up.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-
-#Set up project directory structure
-rootdir = os.path.dirname(os.getcwd())
-datdir = Path(rootdir, "data")
-resdir = Path(rootdir, "results")
-srcdir = Path(rootdir, 'src')
